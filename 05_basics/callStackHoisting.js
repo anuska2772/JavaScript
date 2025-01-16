8593add0d4f35b788f038e5ec2dc803662b8c6f3
anuska2772 <chatterjeeanuska2772@gmail.com>
2025-01-17 00:58:29 +0530

    3rd commit of the day

--- 05_basics/callStackHoisting.js
+++ 05_basics/callStackHoisting.js
@@ -1,9 +1,34 @@
-//hoisting
+// //hoisting
 
-sayMyName("Anuska")
-function sayMyName(finalName){
-    console.log(finalName);
+// sayMyName("Anuska")
+// function sayMyName(finalName){
+//     console.log(finalName);
     
-}//due to hoisting by default the function automatically gets shifted to the top of the code
+// }//due to hoisting by default the function automatically gets shifted to the top of the code
 
-//sayMyName("Anuska")
+// //sayMyName("Anuska")
+//this was function hoisting
+
+//VARIABLE HOISTING
+
+// var age=21
+// console.log(age);
+// var age=21 
+//in this case
+/**
+ * only var age is shifted before console.log.., 
+ * therefore no value is assigned, so o/p=undefined 
+ */
+
+//USING LET AND CONST-> not applicable
+//ONLY APPLICABLE FOR VAR keyqword
+
+//function expression mei nhi chalega
+
+myName()
+var myName=function (){
+    console.log("Hello Anuska!");
+    
+}
+
+
