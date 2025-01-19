f341dd3b5fe9d385a60b80b8e004b1913f777671
anuska2772 <chatterjeeanuska2772@gmail.com>
2025-01-19 19:48:48 +0530

    functionCallStack commited

--- 05_basics/functionCallStack.js
+++ 05_basics/functionCallStack.js
@@ -31,12 +31,12 @@
 
 //3. Return a function
 
-function solve(number){
-    return function(number){
-        return number*number;
-    }
-}
-let ans=solve(100)//argument sent in function solve is unused
+// function solve(number){
+//     return function(number){
+//         return number*number;
+//     }
+// }
+// let ans=solve(100)//argument sent in function solve is unused
 /**
  * The solve function doesn't directly use the 10. 
  * Instead, it simply returns the inner function.
@@ -44,5 +44,58 @@ So, ans now holds a reference to the inner function:
 
 
  */
-let finalAns=ans(10)
-console.log(finalAns);
+// let finalAns=ans(10)
+// console.log(finalAns);
+
+//4. Use function in data structure
+
+// const arr=[
+//     function(a,b){
+//         return a+b;
+//     },
+//     function(a,b){
+//         return a-b;
+//     },
+//     function(a,b){
+//         return a*b;
+//     }
+// ];
+// let first=arr[0];
+// let ans=first(5,10)
+// console.log(ans);
+
+// let second=arr[1];
+// console.log(second(15,10));
+
+// let third=arr[2];
+// console.log(third(5,10));
+
+
+//5. use in an object
+
+// let obj={
+//     age:21,
+//     wt:51,
+//     ht:163,
+//     greet:()=>{
+//         console.log("Hello India");
+        
+//     }
+
+
+// }
+// let greet=obj.greet()
+// console.log(greet);
+// console.log(obj.age);
+
+
+//6. function expression
+
+console.log(greet);
+var greet=function(){
+    console.log("Hello India");
+    
+}
+
+
+
---
+++ 05_basics/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let second=arr[1];
+console.log(second(15,10));
+
+let third=arr[2];
+console.log(third(5,10));
