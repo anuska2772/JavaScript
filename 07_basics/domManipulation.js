f1b4296c51f170892de4ad931cac6acaaa0e68f5
anuska2772 <chatterjeeanuska2772@gmail.com>
2025-01-22 16:42:35 +0530

    DOM BASICS commited

---
+++ 07_basics/domManipulation.js
@@ -0,0 +1,41 @@
+/**
+ * Window object -> global object, ye hierarchy mei sbse aage hota hai, iske upar aur kuch nhi hota
+ * window.open()
+ * window.close()
+ * console.log()  is also under window object
+ * window object is created by browser
+ * DOM- Documnet Object Model
+ */
+/**
+ * We have an html file-> index.html(for eg), we convert it into
+ * JS object. This Js object is called document.
+ * The entire Document Model is called-> Document Object Model(DOM)
+ */
+/**A website-> inspect tools-> console window-> document(press enter)-> all html code displayed */
+/**Anything enclosed within <> is a tag
+ * there is a tokenizer that converts these tags in html file
+ * into tokens, these tokens are converted into-> Nodes
+ * Many such Ndes combine together to form-> DOM
+ * 
+ * <html> ke andar hoti hai <head> and <body>
+ * .: htmls ke 2 child node(head and body) amd <html> is the parent node
+    
+window ke andar ->DOM
+                ->BOM
+                ->JS CORE
+
+BOM-> Browser Object Model, all the interactions that
+happen with the browser,(leaving the html content)-> like history,
+navigation, screen size, etc
+*/
+
+//CODE
+/**
+ * change html
+ * -> access
+ *      -> id
+ *      -> class name
+ *      -> tag name
+ *      -> in response -> multiple output/single output
+ * 
+ */
